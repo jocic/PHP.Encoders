180e1542402c149a462b953f6c3ea9d093a42b3a
Đorđe Jocić <office@djordjejocic.com>
2019-01-04 07:15:24 +0100

    Added unit test for the autoloader class.

---
+++ tests/essentials/test-autoloader.php
@@ -0,0 +1,96 @@
+<?php
+    
+    /*******************************************************************\
+    |* Author: Djordje Jocic                                           *|
+    |* Year: 2019                                                      *|
+    |* License: MIT License (MIT)                                      *|
+    |* =============================================================== *|
+    |* Personal Website: http://www.djordjejocic.com/                  *|
+    |* =============================================================== *|
+    |* Permission is hereby granted, free of charge, to any person     *|
+    |* obtaining a copy of this software and associated documentation  *|
+    |* files (the "Software"), to deal in the Software without         *|
+    |* restriction, including without limitation the rights to use,    *|
+    |* copy, modify, merge, publish, distribute, sublicense, and/or    *|
+    |* sell copies of the Software, and to permit persons to whom the  *|
+    |* Software is furnished to do so, subject to the following        *|
+    |* conditions.                                                     *|
+    |* --------------------------------------------------------------- *|
+    |* The above copyright notice and this permission notice shall be  *|
+    |* included in all copies or substantial portions of the Software. *|
+    |* --------------------------------------------------------------- *|
+    |* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, *|
+    |* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES *|
+    |* OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND        *|
+    |* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT     *|
+    |* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,    *|
+    |* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, RISING     *|
+    |* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR   *|
+    |* OTHER DEALINGS IN THE SOFTWARE.                                 *|
+    \*******************************************************************/
+    
+    use PHPUnit\Framework\TestCase;
+    
+    /**
+     * <i>TestAutoloader</i> class is used for testing project's autoloader as
+     * it isn't tested automatically by Travis CI.
+     * 
+     * @author    Djordje Jocic <office@djordjejocic.com>
+     * @copyright 2018 All Rights Reserved
+     * @version   1.0.0
+     */
+    
+    class TestAutoloader extends TestCase
+    {
+        /*********************\
+        |* GET & SET METHODS *|
+        \*********************/
+        
+        // GET & SET METHODS GO HERE
+        
+        /*****************\
+        |* CHECK METHODS *|
+        \*****************/
+        
+        // CHECK METHODS GO HERE
+        
+        /*******************\
+        |* PRIMARY METHODS *|
+        \*******************/
+        
+        /**
+         * Tests the project's autoloading function.
+         * 
+         * @author    Djordje Jocic <office@djordjejocic.com>
+         * @copyright 2018 All Rights Reserved
+         * @version   1.0.0
+         * 
+         * @return void
+         */
+        
+        public function testFunction()
+        {
+            // Core Variables
+            
+            $testValues = [
+                "\Jocic\Encoders\Base\Base32" => true,
+                "\Jocic\Encoders\BAse\Potato" => false,
+            ];
+            
+            // Logic
+            
+            foreach ($testValues as $testValue => $testResult)
+            {
+                $this->assertSame($testResult,
+                    load_encoders_class($testValue), $testValue);
+            }
+        }
+        
+        /*********************\
+        |* SECONDARY METHODS *|
+        \*********************/
+        
+        // SECONDARY METHODS GO HERE
+    }
+    
+?>
